189f9cd4c6e9862adf1102b5bab84a5a8c2f525f
Aman Saxena <saxenaaman628@gmail.com>
2025-04-30 16:39:57 +0530

    updating middlewares

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -1,14 +1,58 @@
 package handlers
 
 import (
+	"log"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
-	r := gin.Default()
-
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(CORSMiddleware())   //CORS might need to update for prod
+	r.Use(LoggerMiddleware()) //logger
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Welcome to Kava Life ERP Backend (Gin Setup)"})
 	})
 	return r
+}
+
+func LoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		startTime := time.Now()
+
+		// Process request
+		c.Next()
+
+		// After request
+		duration := time.Since(startTime)
+		statusCode := c.Writer.Status()
+		method := c.Request.Method
+		path := c.Request.URL.Path
+		clientIP := c.ClientIP()
+
+		log.Printf("[GIN] %3d | %13v | %15s | %-7s %s",
+			statusCode,
+			duration,
+			clientIP,
+			method,
+			path,
+		)
+	}
+}
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
