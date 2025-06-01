3f8650bbd4feb21fd93170f9acab7204b8e4769a
Aman Saxena <saxenaaman628@gmail.com>
2025-06-01 23:07:02 +0530

    User api setup

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -3,8 +3,11 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func Routes(r *gin.Engine) {
-	api := r.Group("/api/v1")
+
+	api := r.Group("/api")
+
 	api.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
+	api.GET("/allUsers", AllUsers)
 }
