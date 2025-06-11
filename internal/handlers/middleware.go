bd4ff217f67376a02d0590d6c2ba998552687bb6
Parth Mehta <parthpmehta23@gmail.com>
2025-06-11 15:37:31 +0530

    Implement CORS middleware with allowed origins and add user check and logout endpoints

--- internal/handlers/middleware.go
+++ internal/handlers/middleware.go
@@ -22,18 +22,25 @@ func RunApp() *gin.Engine {
 	return r
 }
 
+var allowedOrigins = map[string]bool{
+	"http://localhost:5173":   true,
+	"https://erp.kavalife.in": true,
+}
+
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		origin := c.Request.Header.Get("Origin")
+		if allowedOrigins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
+			if c.Request.Method == "OPTIONS" {
+				c.AbortWithStatus(204)
+				return
+			}
 		}
-
 		c.Next()
 	}
 }
